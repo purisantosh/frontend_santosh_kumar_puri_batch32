5ae997e1c47247bd04193cae4dc158236d8e2913
purisantosh <116016459+purisantosh@users.noreply.github.com>
2023-06-05 09:41:08 -0500

    Adding 06-05-2023

---
+++ 06-02-2023/abc.js
@@ -0,0 +1,59 @@
+let hobbies = ["Playing Guiter", "Playing Cricket"]
+
+
+function init() {
+    let hobbySelect = document.getElementById('hobby-list');
+    hobbySelect.innerHTML = "";
+    for (let i = 0; i < hobbies.length; i++) {
+        hobbySelect.options[hobbySelect.options.length] = new Option(hobbies[i]);
+    }
+
+    let delhobbySelect = document.getElementById('hobby-list-del');
+    delhobbySelect.innerHTML = "";
+    for (let i = 0; i < hobbies.length; i++) {
+        delhobbySelect.options[delhobbySelect.options.length] = new Option(hobbies[i]);
+    }
+
+}
+init();
+
+
+
+function getAllHobbies() {
+    return hobbies;
+}
+
+function addNewHobby() {
+    let newHobby = document.getElementById('add-hobby').value
+    newHobby=newHobby.trim();
+    let flag=false;
+    for(let i=0;i<hobbies.length;i++){
+        if((hobbies[i]).toLocaleLowerCase()==newHobby.toLocaleLowerCase()){
+            flag=true;
+            break;
+        }
+    }
+    if(!flag){
+        hobbies.push(newHobby)
+        init();
+        alert(`${newHobby} has been added sucessfully!!!`)
+    }else{
+        alert(`${newHobby} is already in hobbies list!!!`)
+    }
+  
+}
+
+function removeHobbyFromList() {
+    let e = document.getElementById("hobby-list-del");
+    let hobby = e.options[e.selectedIndex].value;
+    let temp = []
+    for (let i = 0; i < hobbies.length; i++) {
+        if (hobby != hobbies[i]) {
+            temp.push(hobbies[i])
+        }
+    }
+    hobbies = temp;
+
+    init();
+    alert(`${hobby} has been deleted successfully!`)
+}
